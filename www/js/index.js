0c586374108419640bb257ca65f871f3b4396798
pit park <yanx1_15@uni.worc.ac.uk>
2016-11-10 16:06:29 +0000

    adsada

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,34 @@
 $(document).on("pagecreate","#pageone",function(){
   $('#submitButton').on("click", function(){
     submitText();
-  });            
+  });   
+  $('#submitButton-form').on("click", function(){
+    submitForm();
+  });   
 });            
-
 
 function submitText() {
 	var text = $('#textinput').val();
-	alert(text);
+    storeValue("name",text);
+	window.alert(text);
+}
+
+function submitForm() {
+    var name = $('#name').val();
+    storeValue("name",text);
+    var email = $('#email').val();
+    storeValue("email",text);
+    var tel = $('#tel').val();
+    storeValue("tel",text);
+    var date = $('#date').val();
+    storeValue("date",text);
+    var psw = $('#psw').val();
+    storeValue("psw",text);
+    window.alert(name);
 }
 
 function storeValue(key, value) {
 	//add some code to store the key-value pair in persistant storage 
+    window.localStorage.setItem(key,value);
+   
 }
